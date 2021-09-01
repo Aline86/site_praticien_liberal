840f1b880d97ef8930df4828ef3220389d4cbcc6
Claire-Aline HAESTIE <claire-aline@systeos.fr>
2021-09-01 14:56:45 +0200

    change href

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ function init() {
 }
 window.onscroll = function() {
     myFunction()
+    
 };
 
 function myFunction(){
     var container = document.querySelector(".container");
+   
     if (window.pageYOffset < container.offsetTop ) {
         document.querySelector(".nav_pc").style.backgroundColor="rgb(17, 112, 37)"
         document.querySelector(".nav_pc").style.backgroundImage = "none"
@@ -25,5 +27,22 @@ function myFunction(){
     else
     {
         document.querySelector(".nav_pc").style.backgroundImage = "url('menu.jpg')"
+    }
+    setHref("accueil", "background")
+    setHref("practice", "pratique")
+    setHref("therapy", "therapie")
+    setHref("cabi", "cabinet")
+    setHref("cont", "contact")
+
+}
+
+function setHref(idhref, idcontainer){
+    if (window.pageYOffset >= document.getElementById(idcontainer).offsetTop ) {
+        document.querySelectorAll('.a').forEach(elem =>{
+            elem.style.fontWeight = "lighter"
+            elem.style.textDecoration = "none"
+        })
+        document.getElementById(idhref).style.fontWeight = "bold"
+        document.getElementById(idhref).style.textDecoration = "underline 2px"
     }
 }
