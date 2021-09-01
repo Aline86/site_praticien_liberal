86ebdf744aa3c3613be376ac217a4d9a5376f03b
Claire-Aline HAESTIE <claire-aline@systeos.fr>
2021-09-01 14:14:15 +0200

    change_menubar

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function init() {
         })
     })
 }
+window.onscroll = function() {
+
+
+    myFunction()
+};
+
+function myFunction(){
+    var container = document.querySelector(".container");
+    if (window.pageYOffset < container.offsetTop ) {
+        document.querySelector(".nav_pc").style.backgroundColor="rgb(17, 112, 37)"
+        document.querySelector(".nav_pc").style.backgroundImage = "none"
+    }
+    else
+    {
+        document.querySelector(".nav_pc").style.backgroundImage = "url('menu.jpg')"
+    }
+}
