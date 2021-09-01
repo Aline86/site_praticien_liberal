1551ba66651760115ef93549fa5bc8389a98955a
Claire-Aline HAESTIE <claire-aline@systeos.fr>
2021-09-01 13:52:06 +0200

    click

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 window.onload = init
 function init() {
     document.querySelectorAll('.a').forEach(elem =>{
-        console.log(elem)
         elem.addEventListener('click', function(){
+            document.querySelectorAll('.a').forEach(elem =>{
+                elem.style.fontWeight = "lighter"
+                elem.style.textDecoration = "none"
+            })
+            elem.style.fontWeight = "bold"
+            elem.style.textDecoration = "underline 2px"
             $("#menu-btn").prop('checked', false);
         })
     })
