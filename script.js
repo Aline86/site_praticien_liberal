0c6db1b52a365f4b4f8d40760a050c6b73629d61
Aline86 <ca.haestie@gmail.com>
2021-09-05 23:05:06 +0200

    changements_minimes

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 window.onload = init
 function init() {
+    hover_under_after_click()
     document.querySelectorAll('.a').forEach(elem =>{
-        elem.addEventListener('mouseenter', function(){
-            if(elem.style.textDecoration == "none")
-            {
-                elem.classList.add('underline')
-            }
-            else
-            {
-                elem.classList.remove('underline')
-            }
-            
-        })
-        elem.addEventListener('click', function(){
+        elem.addEventListener('click', function(e){
             document.querySelectorAll('.a').forEach(elem =>{
                 elem.style.fontWeight = "lighter"
                 elem.style.textDecoration = "none"
@@ -20,12 +10,12 @@ function init() {
             elem.style.fontWeight = "bold"
             elem.style.textDecoration = "underline 1.5px"
             $("#menu-btn").prop('checked', false);
+            
         })
     })
 }
 window.onscroll = function() {
     myFunction()
-    init()
     if(document.querySelector('.docto2') !=  undefined)
     {
         if(window.pageYOffset >= document.querySelector('.docto2').offsetTop + 50)
@@ -35,13 +25,31 @@ window.onscroll = function() {
         else
         {
             document.querySelector('.docto2').style.display = "block"
-        }
-        
+        } 
     }
 };
-
+function hover_under_after_click(){
+    document.querySelectorAll('.a').forEach(elem =>{
+        elem.addEventListener('mouseenter', function(){
+            if(elem.style.textDecoration == "underline 1.5px" && elem.style.textDecoration != "none")
+            {
+                elem.classList.remove('underline')   
+            }
+            else {
+                elem.classList.add('underline')
+            } 
+        })
+    })
+}
 function myFunction(){
     var container = document.querySelector(".container");
+    if(document.getElementById("pratique").offsetTop >= window.pageYOffset)
+    {
+        document.querySelectorAll('.a').forEach(elem =>{
+            elem.style.fontWeight = "lighter"
+            elem.style.textDecoration = "none" 
+        }) 
+    }
     setHref("practice", "pratique")
     setHref("therapy", "therapie")
     setHref("cabi", "cabinet")
@@ -49,13 +57,15 @@ function myFunction(){
 }
 
 function setHref(idhref, idcontainer){
-    if (window.pageYOffset + 10 >= document.getElementById(idcontainer).offsetTop) {
+    if (window.pageYOffset + 10 >= document.getElementById(idcontainer).offsetTop ) {
         document.querySelectorAll('.a').forEach(elem =>{
             elem.style.fontWeight = "lighter"
             elem.style.textDecoration = "none" 
-           
         })
         document.getElementById(idhref).style.fontWeight = "bold"
-        document.getElementById(idhref).style.textDecoration = "underline 1.5px"
+        document.getElementById(idhref).style.textDecoration = "underline 1.5px"   
     }
 }
+$( document ).ready(function() {
+    hover_under_after_click()
+});
